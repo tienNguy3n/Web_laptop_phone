d41b97cb722902ef59e5d68ed6794f053e54d5ab
Tien <tienhype@gmail.com>
2022-11-18 23:25:04 +0700

    add new file

---
+++ assets/js/slider.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function () {
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function () {
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function () {
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+    $(".item5").click(function () {
+        $("#myCarousel").carousel(4);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function () {
+        $("#myCarousel").carousel("next");
+    });
+});
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("top");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
