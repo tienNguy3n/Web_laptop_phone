8476c4d0e7904c6bd91a8bb4ff5043371ab5692e
Tien <tienhype@gmail.com>
2022-12-19 17:33:19 +0700

    update Buy button

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,37 @@
 const loginBtn = document.querySelector('.js-login')
-const modal = document.querySelector('.js-modal')
-const loginClose = document.querySelector('.js-close')
-const loginContainer = document.querySelector('.js-container')
+const buyBtn = document.querySelector('.js-buy')
+const loginModal = document.querySelector('.js-modal-login')
+const buyModal = document.querySelector('.js-modal-buy')
+const loginClose = document.querySelector('.js-close-login')
+const buyClose = document.querySelector('.js-close-buy')
+const loginContainer = document.querySelector('.js-container-login')
+const buyContainer = document.querySelector('.js-container-buy')
 
 function showLogin() {
-    modal.classList.add('open')
+    loginModal.classList.add('open')
+}
+
+function showBuy() {
+    buyModal.classList.add('open')
 }
 
 function hideLogin() {
-    modal.classList.remove('open')
+    loginModal.classList.remove('open')
+}
+
+function hideBuy() {
+    buyModal.classList.remove('open')
 }
 
 loginBtn.addEventListener('click', showLogin)
+buyBtn.addEventListener('click', showBuy)
 loginClose.addEventListener('click', hideLogin)
-modal.addEventListener('click', hideLogin)
+buyClose.addEventListener('click', hideBuy)
+loginModal.addEventListener('click', hideLogin)
+buyModal.addEventListener('click', hideBuy)
 loginContainer.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+buyContainer.addEventListener('click', function (event) {
     event.stopPropagation()
 })
